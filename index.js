b4f16991bb5f9daa639f6c15004a25d0e253380b
Noah A Klem <noahaklem@gmail.com>
2021-11-09 17:06:46 -0700

    commit tree 1c1f063e5e1257a18737c5266b0c9af374e028f8

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+let totalBatteries;
+
+batteryBatches.reduce(function(total, number){
+    return totalBatteries = total + number
+}, 0)
+
